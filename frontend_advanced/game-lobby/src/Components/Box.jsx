d17fe04c8adecfa4b4900868ff7268ee7c1787ab
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-05-03 10:22:41 -0400

    Change color

--- frontend_advanced/game-lobby/src/Components/Box.jsx
+++ frontend_advanced/game-lobby/src/Components/Box.jsx
@@ -7,7 +7,7 @@ const Box = (props) => {
     const {users} = useContext(Context);
     console.log("users: ", users)
     return (
-        <div className="outside">
+        <div className="outside" style={{background: users[props.id].color}}>
             <div className="title"><h1>P{props.id + 1}</h1></div>
             <SelectButton id={props.id}/>
         </div>
