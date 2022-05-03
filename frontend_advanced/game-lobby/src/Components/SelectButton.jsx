f5ded1d8b914a21d77b214c560c15abeb112398d
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-05-03 11:25:59 -0400

    resolve some warnings

--- frontend_advanced/game-lobby/src/Components/SelectButton.jsx
+++ frontend_advanced/game-lobby/src/Components/SelectButton.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, InputLabel, MenuItem, FormControl, Select} from '@material-ui/core';
+import { MenuItem, FormControl, Select} from '@material-ui/core';
 import "./../theme/SelectButton.css";
 import Context from "./../Context/context";
 
@@ -13,12 +13,12 @@ export default function SelectButton(props) {
         let curr = 0;
 
         for(i = 0; i < users.length; i++){
-          if(users[i].color == event.target.value){
+          if(users[i].color === event.target.value){
             return;
           }
         }
         for(i = 0; i < users.length; i++){
-            if(users[i].id == props.id){
+            if(users[i].id === props.id){
                 acc.push({id: users[i].id, color: event.target.value});
                 break;
             }
@@ -36,13 +36,14 @@ export default function SelectButton(props) {
       <FormControl variant="outlined" size='medium' margin='normal'>
         <Select
           variant="filled"
-          value={users[props.id]}
+          value={users[props.id].color}
           label="Color"
           onChange={handleChange}
         >
           <MenuItem value={"blue"}>Blue</MenuItem>
           <MenuItem value={"red"}>Red</MenuItem>
           <MenuItem value={"yellow"}>Yellow</MenuItem>
+          <MenuItem value={"green"}>Green</MenuItem>
         </Select>
       </FormControl>
   );
