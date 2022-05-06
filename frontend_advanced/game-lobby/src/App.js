8a70b2bebdb3f31e51c460d3c089f15a61982a47
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-05-06 19:26:45 -0400

    define the location of global css

--- frontend_advanced/game-lobby/src/App.js
+++ frontend_advanced/game-lobby/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./theme/App.css";
 import GameLoby from "./Components/GameLoby/game_loby";
 import Helloworld from "./Components/Helloworld";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
